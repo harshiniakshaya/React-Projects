77a080edb407e290fc3f8119f9b49b6afea90d18
harshiniakshaya <harshiniakshaya@gmail.com>
2024-03-02 22:59:59 +0530

    Currency Converter App

---
+++ Currency-Converter-App/src/App.jsx
@@ -0,0 +1,11 @@
+import { CurrencyConverter } from './components/CurrencyConverter'
+
+function App() {
+  return (
+    <>
+      <CurrencyConverter />
+    </>
+  )
+}
+
+export default App
---
+++ Currency-Converter-App/src/components/CurrencyConverter.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import "../styles/CurrencyConverter.css";
+import axios from "axios";
+
+export const CurrencyConverter = () => {
+    const[amount, setAmount] = useState(1);
+    const[fromCurrency,setFromCurrency] = useState("USD");
+    const[toCurrency,setToCurrency] = useState("INR");
+    const[convertedAmount, setConvertedAmount] = useState(null);
+    const[exchangeRate,setExchangeRate] = useState(null);
+
+    
+
+    useEffect(()=>{
+        const getExchangedRate = async()=>{
+            try{
+                let url = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+                const response=await axios.get(url);
+                //console.log(response);
+                setExchangeRate(response.data.rates[toCurrency]);
+            }
+            catch(error){
+                console.error("Error fetching exchange rate:",error)
+            }
+        };    
+    
+        getExchangedRate(); 
+        },[fromCurrency, toCurrency]
+    );
+
+    useEffect(()=>{
+        if(exchangeRate!==null){
+            setConvertedAmount((amount*exchangeRate).toFixed(2));
+        }
+    },[amount,exchangeRate])
+
+    const handleAmountChange = (e) =>{
+        const value = parseFloat(e.target.value);
+        setAmount(isNaN(value)?0:value);
+    }
+
+    const handleFromCurrencyChange = (e) =>{
+        setFromCurrency(e.target.value);
+    }
+
+    const handleToCurrencyChange = (e) =>{
+        setToCurrency(e.target.value);
+    }
+
+  return (
+    <div className='currency-converter'>
+        <div className='box'></div>
+        <div className='data'>
+            <h1>Currency Converter</h1>
+            <div className='input-container'>
+                <label htmlFor='amt'>Amount:</label>
+                <input type='number' id='amt' value={amount}
+                    onChange={handleAmountChange}
+                />
+            </div>
+
+            <div className='input-container'>
+                <label htmlFor='fromCurrency'>From Currency:</label>
+                <select id='fromCurrency' value={fromCurrency}
+                    onChange={handleFromCurrencyChange}
+                >
+                    <option value="USD">USD - United States Dollar</option>
+                    <option value="EUR">EUR - Euro</option>
+                    <option value="GBP">GBP - British Pouund Sterling</option>
+                    <option value="JPY">JPY - Japanese yen</option>
+                    <option value="AUD">AUD - Australian Dollar</option>
+                    <option value="CAD">CAD - Canadian Dollar</option>
+                    <option value="CNY">CNY - Chinese Yuan</option>
+                    <option value="INR">INR - Indian Rupee</option>
+                    <option value="BRL">BRL - Brazilian Real</option>
+                    <option value="ZAR">ZAR - South African Rand</option>
+                </select>
+            </div>
+
+            <div className='input-container'>
+                <label htmlFor='toCurrency'>To Currency:</label>
+                <select id='toCurrency' value={toCurrency}
+                    onChange={handleToCurrencyChange}
+                >
+                    <option value="USD">USD - United States Dollar</option>
+                    <option value="EUR">EUR - Euro</option>
+                    <option value="GBP">GBP - British Pouund Sterling</option>
+                    <option value="JPY">JPY - Japanese yen</option>
+                    <option value="AUD">AUD - Australian Dollar</option>
+                    <option value="CAD">CAD - Canadian Dollar</option>
+                    <option value="CNY">CNY - Chinese Yuan</option>
+                    <option value="INR">INR - Indian Rupee</option>
+                    <option value="BRL">BRL - Brazilian Real</option>
+                    <option value="ZAR">ZAR - South African Rand</option>
+                </select>
+            </div>
+
+            <div className='result'>
+                <p>{amount} {fromCurrency} is equal to {convertedAmount} {toCurrency}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
